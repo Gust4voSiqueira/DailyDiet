258d426f0eae07f82e174203dfe7eb4962e3af6b
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-09-28 11:45:11 -0300

    Adicionando temas globais

---
+++ src/theme/index.ts
@@ -0,0 +1,32 @@
+export default {
+  COLORS: {
+    WHITE: '#FFF',
+
+    GRAY_1: '#1B1D1E',
+    GRAY_3: '#5C6265',
+    GRAY_4: '#B9BBBC',
+    GRAY_5: '#DDDEDF',
+    GRAY_6: '#EFF0F0',
+    GRAY_7: '#FAFAFA',
+
+    GREEN_LIGHT: '#35F0DB',
+    GREEN_MID: '#CBE4B4',
+    GREEN_DARK: '#639339',
+
+    RED_LIGHT: '#F4E6E7',
+    RED_MID: '#F3BABD',
+    RED_DARK: '#BF3B44',
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Nunito_400Regular',
+    BOLD: 'Nunito_700Bold',
+  },
+  FONT_SIZE: {
+    SM: 12,
+    XSM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XXL: 32,
+  },
+}
