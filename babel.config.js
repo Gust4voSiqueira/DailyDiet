be56a3e72a3a4840c56fbd1bf602203dba0ceb7a
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-09-28 11:44:20 -0300

    Mapeando pastas

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,19 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@theme': './src/theme/index.ts',
+            '@components': './src/components/index.ts',
+            '@screens': './src/screens/index.ts',
+          },
+        },
+      ],
+    ],
+  }
+}
