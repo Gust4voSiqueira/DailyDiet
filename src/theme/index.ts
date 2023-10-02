aafd7ef0967b044c744368209c4e7ddbeea416f8
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-02 14:59:13 -0300

    Adicionando novo fontSize

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,14 +2,15 @@ export default {
   COLORS: {
     WHITE: '#FFF',
 
-    GRAY_1: '#1B1D1E',
-    GRAY_3: '#5C6265',
-    GRAY_4: '#B9BBBC',
-    GRAY_5: '#DDDEDF',
-    GRAY_6: '#EFF0F0',
-    GRAY_7: '#FAFAFA',
+    GRAY_100: '#1B1D1E',
+    GRAY_200: '#333638',
+    GRAY_300: '#5C6265',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#DDDEDF',
+    GRAY_600: '#EFF0F0',
+    GRAY_700: '#FAFAFA',
 
-    GREEN_LIGHT: '#35F0DB',
+    GREEN_LIGHT: '#E5F0DB',
     GREEN_MID: '#CBE4B4',
     GREEN_DARK: '#639339',
 
@@ -26,6 +27,7 @@ export default {
     XSM: 14,
     MD: 16,
     LG: 18,
+    XLG: 20,
     XL: 24,
     XXL: 32,
   },
