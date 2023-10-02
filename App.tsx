6f9b1796d1546487c7abebe6bc80aae422020165
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-02 14:54:20 -0300

    Ajuste importações

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable camelcase */
 import { StatusBar } from 'expo-status-bar'
-import { Routes } from './src/routes'
 import {
   useFonts,
   Nunito_400Regular,
   Nunito_700Bold,
 } from '@expo-google-fonts/nunito'
-import { Loading } from './src/components'
+import { Routes } from './src/routes'
+import { Loading } from '@components/index'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Nunito_400Regular, Nunito_700Bold })
