97245132f533c083ce62a2be3f2cbe615c9303ad
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-02 14:53:21 -0300

    Mapeando novas rotas

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -2,6 +2,19 @@ export declare global {
   namespace ReactNavigation {
     interface RootParamList {
       home: undefined
+      general: {
+        percentage: number
+      }
+      new: undefined
+      meal: {
+        idMeal: string
+      }
+      edit: {
+        meal: string
+      }
+      created: {
+        isDiet: boolean
+      }
     }
   }
 }
--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,5 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { Home } from '../screens'
+
+import {
+  GeneralStatistics,
+  Home,
+  NewMeal,
+  MealDetails,
+  EditMeal,
+  CreatedMeal,
+} from '@screens/index'
 
 const { Navigator, Screen } = createNativeStackNavigator()
 
@@ -7,6 +15,11 @@ export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }} initialRouteName="home">
       <Screen name="home" component={Home} />
+      <Screen name="general" component={GeneralStatistics} />
+      <Screen name="new" component={NewMeal} />
+      <Screen name="meal" component={MealDetails} />
+      <Screen name="edit" component={EditMeal} />
+      <Screen name="created" component={CreatedMeal} />
     </Navigator>
   )
 }
