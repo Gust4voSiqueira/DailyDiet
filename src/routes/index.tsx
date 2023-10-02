e4bb408321e92b80f9827964e1226340e9e213b0
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2023-10-02 14:56:29 -0300

    Estilizando troca de telas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
+import { View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { AppRoutes } from './app.routes'
-import { View } from 'react-native'
+import theme from '@theme/index'
 
 export function Routes() {
   return (
-    <View>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.COLORS.GRAY_700,
+      }}
+    >
       <NavigationContainer>
         <AppRoutes />
       </NavigationContainer>
